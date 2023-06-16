88052c2728e1498143e992f54902fe9caba14b87
sandhya <114242237+sandhyakandari@users.noreply.github.com>
2023-06-16 12:55:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 // complete the given function
 
 function palindrome(str){
-
+  let s="";
+	for(let i=0;i<str.length;i++){
+		  if(str[i]!=' '){
+			  s=s+str[i];
+		  }
+	} let i=0; 
+	let r=s.length-1;
+	while(i<r){
+		if(s[i]!=s[r]){
+			return false;
+		}
+		i++; 
+		r--;
+	}
+return true;
+	
 }
 module.exports = palindrome
